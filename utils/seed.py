068e5d8cdcd0c416102b80d3d9a93a7bfe0b289f
shahramtaheri <68184774+shahramtaheri@users.noreply.github.com>
2025-12-18 16:50:10 +0300

    Create seed.py

---
+++ utils/seed.py
@@ -0,0 +1,9 @@
+import os, random
+import torch
+
+def seed_everything(seed: int = 42):
+    random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.benchmark = True
